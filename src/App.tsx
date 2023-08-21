15d2507491135093f39896668a6b1bd3aced6613
Cristina Jung <cristijung@gmail.com>
2023-08-21 13:00:30 -0300

    base pronta

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,24 @@
-
+import React from 'react';
+import { useSelector, useDispatch } from 'react-redux';
+import { increment, decrement, reset } from './store/counterSlice';
 import './App.css';
 
 function App() {
+  const count = useSelector((state: any) => state.counter.value);
+  const dispatch = useDispatch();
+
   return (
-   <>
-   </>
+    <div className="App">
+      <header className="App-header">
+        <h1>Redux Toolkit Counter</h1>
+        <p>Count: {count}</p>
+        <div>
+          <button onClick={() => dispatch(increment())}>Increment</button>
+          <button onClick={() => dispatch(decrement())}>Decrement</button>
+          <button onClick={() => dispatch(reset())}>Reset</button>
+        </div>
+      </header>
+    </div>
   );
 }
 
--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,8 @@
-import React from 'react';
+
 import ReactDOM from 'react-dom/client';
 import './index.css';
+import store from './store/store';
+import { Provider } from 'react-redux'
 import App from './App';
 
 
@@ -8,8 +10,7 @@ const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
 root.render(
-  <React.StrictMode>
+  <Provider store={store}>
     <App />
-  </React.StrictMode>
+ </Provider>
 );
-
